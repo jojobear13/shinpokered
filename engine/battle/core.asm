d0ca2041cc318f1a7e97705cb04a0abd923d0772
wisp92 <josephsakos@gmail.com>
2020-06-12 21:33:44 +0800

    Added StorePKMNLevels in stats_functions.asm and freed-up bank space core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -223,34 +223,7 @@ SetScrollXForSlidingPlayerBodyLeft:
 StartBattle:	;joedebug - start of the battle
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; wispnote - Store PKMN Levels at the Beggining of the Battle.
-	xor a
-	ld [wMonDataLocation], a
-	ld [wWhichPokemon], a
-	ld hl, wPartyCount
-	ld de, wStartBattleLevels
-	push de
-	push hl
-.loopStorePKMNLevels
-	pop hl
-	inc hl
-	ld a, [hl]
-	cp $ff
-	jp z, .doneStorePKMNLevels
-	push hl
-	call LoadMonData
-	pop hl
-	pop de
-	ld a, [wWhichPokemon]
-	inc a
-	ld [wWhichPokemon], a
-	ld a, [wLoadedMonLevel]
-	ld [de], a
-	inc de
-	push de
-	push hl
-	jp .loopStorePKMNLevels
-.doneStorePKMNLevels
-	pop de	
+	callba StorePKMNLevels
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	xor a
 	ld [wPartyGainExpFlags], a
