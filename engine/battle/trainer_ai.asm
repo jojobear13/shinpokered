162872283b373e4c6c0b10ebd76f79701eb52f9c
jojobear13 <jojothebear13@gmail.com>
2026-03-17 19:20:54 -0500

    fix missing calls

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -2085,7 +2085,7 @@ AIUseXAttack:
 	jr AIIncreaseStat
 
 AIUseXDefend:
-	AIUseItemStrategyCheck_Physical
+	call AIUseItemStrategyCheck_Physical
 	ret nc
 	ld b, $B
 	ld a, X_DEFEND
@@ -2099,7 +2099,7 @@ AIUseXSpeed:
 	jr AIIncreaseStat
 
 AIUseXSpecial:
-	AIUseItemStrategyCheck_Special
+	call AIUseItemStrategyCheck_Special
 	ret nc
 	ld b, $D
 	ld a, X_SPECIAL
